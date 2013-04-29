f295a54d0ff0ec650a59225af8e416ec896f127d
ThunderBoomer <CarrollCEC@gmail.com>
2013-04-29 13:34:46 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ADSUM2::Application.config.secret_token = 'acfc36f594c00509d96253b9247bd0d474f1d4a0d423e3ff18362ac103a1d815c20346d73e24d03c94d4b8b398b9ba6427b94d266c91cb52f2b0813d4bf67bc4'
