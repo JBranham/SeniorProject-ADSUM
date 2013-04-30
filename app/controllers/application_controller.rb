ef332861d18573bd9ae0fee86d3354539b860626
ThunderBoomer <CarrollCEC@gmail.com>
2013-04-29 23:39:07 -0400

    looking better all the time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,20 @@
 class ApplicationController < ActionController::Base
   before_filter :authorize
+  before_filter :detect_user
   protect_from_forgery
 
   protected
+  
 
     def authorize
       unless User.find_by_id(session[:user_id])
         redirect_to login_url, notice: "Please log in"
+      end
+    end
+    
+    def detect_user
+      if session[:user_id]
+        @user = User.find_by_id(session[:user_id])
       end
     end
     
