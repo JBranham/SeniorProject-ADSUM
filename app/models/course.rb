c027dab8d89ee6c829c6f7f35500e33ed7f27abb
ThunderBoomer <CarrollCEC@gmail.com>
2013-04-30 01:53:22 -0400

    edit attendance

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,8 @@
 class Course < ActiveRecord::Base
-  attr_accessible :name, :user_id, :f_day, :l_day, :course_id, :pass_d_points_attributes, :lecture_days_attributes, :quick_points_attributes, :designated_points_attributes, :students_attributes, :attendances_attributes
+  attr_accessible :name, :user_id, :f_day, :l_day, :course_id, :pass_d_points_attributes, :lecture_days_attributes, :designated_points_attributes, :students_attributes, :attendances_attributes
   has_many :students, dependent: :destroy
-  has_many :quick_points, dependent: :destroy
   has_many :designated_points, dependent: :destroy
   has_many :attendances, dependent: :destroy
-  has_many :pass_q_points, dependent: :destroy
   has_many :pass_d_points, dependent: :destroy
   has_many :lecture_days, dependent: :destroy
   belongs_to :user
@@ -13,9 +11,7 @@ class Course < ActiveRecord::Base
     :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 
   accepts_nested_attributes_for :attendances
-  accepts_nested_attributes_for :pass_q_points
   accepts_nested_attributes_for :lecture_days
-  accepts_nested_attributes_for :quick_points
   accepts_nested_attributes_for :designated_points
   accepts_nested_attributes_for :pass_d_points
   
